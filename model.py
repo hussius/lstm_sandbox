33a87f6f7ecf0ca4b2f66542924496b4540edf55
hussius <mikael@peltarion.com>
2018-03-06 13:06:57 +0100

    First version with simple LSTM, seq2seq, attention

---
+++ model.py
@@ -0,0 +1,36 @@
+from keras.models import Sequential
+from keras.layers import TimeDistributed, RepeatVector, \
+                         LSTM, Dense
+
+from attention_decoder import AttentionDecoder
+
+def lstm(lstm_cells, n_timesteps_in, n_features):
+    model = Sequential(name='SimpleLSTM')
+    model.add(LSTM(lstm_cells, input_shape=(n_timesteps_in, n_features),
+              return_sequences=True))
+    model.add(TimeDistributed(Dense(n_features, activation='softmax')))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+    print(model.summary())
+    return(model)
+
+
+def seq2seq(lstm_cells, n_timesteps_in, n_features):
+    model = Sequential(name="Seq2Seq")
+    model.add(LSTM(lstm_cells, input_shape=(n_timesteps_in, n_features)))
+    # The final state is repeated five times to match the number of time steps
+    # in the output
+    model.add(RepeatVector(n_timesteps_in))
+    model.add(LSTM(lstm_cells, return_sequences=True))
+    model.add(TimeDistributed(Dense(n_features, activation='softmax')))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+    print(model.summary())
+    return(model)
+
+
+def attention(lstm_cells, n_timesteps_in, n_features):
+    model = Sequential(name="AttentionLSTM")
+    model.add(LSTM(lstm_cells, input_shape=(n_timesteps_in, n_features), return_sequences=True))
+    model.add(AttentionDecoder(lstm_cells, n_features))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+    print(model.summary())
+    return(model)
